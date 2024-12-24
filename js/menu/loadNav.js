c8c7ac9980d0cf592df1f85359d62da4513233d0
limp bizkit <slow33@gmail.com>
2024-12-24 22:47:24 +0900

    almost done aside menu handling

--- js/menu/loadNav.js
+++ js/menu/loadNav.js
@@ -1,26 +1,30 @@
 function loadXML(src) {
    return new Promise(function (resolve, reject) {
       let xhr = new XMLHttpRequest();
-      xhr.open('GET', src, true);
-      xhr.responseType = 'text';
-      xhr.setRequestHeader('Accept', 'text/plain');
-      xhr.send();
-      xhr.onload = () => resolve(xhr.responseText);
-      xhr.onerror = () => reject(xhr.responseText);
+      if(src){
+         xhr.open('GET', src, true);
+         xhr.responseType = 'text';
+         xhr.setRequestHeader('Accept', 'text/plain');
+         xhr.send();
+         xhr.onload = () => resolve(xhr.responseText);
+         xhr.onerror = () => reject(xhr.responseText);
+      }
    });
 }
 
 async function loadMenu(){
    let navText = await loadXML('/page/components/nav.html');
    let nav = await setNavEl(navText);
-   let asideUrl = await selectAsideUrl(nav);
+   let asideUrl = await selectAsideUrl();
    let asideText = await loadXML(asideUrl);
    let aside = await setAside(asideText);
-   setCurrentUrl(aside, asideUrl, nav);
+   document.querySelectorAll('aside ul li ul')
+      .forEach(toggle => toggle.hidden = true);
+   setCurrentUrl(aside, nav);
+   toggleByUrl(aside);
    aside.addEventListener('click', toggleHide);
 }
-let promise = loadMenu();
-promise.then(result => result);
+loadMenu().catch(error => new Error(error.message));
 
 async function setNavEl(text){
    let nav = document.createElement('nav');
@@ -31,63 +35,41 @@ async function setNavEl(text){
    return nav
 }
 
-async function selectAsideUrl (nav) {
-   let asideUrl;
+async function selectAsideUrl () {
    if (location.href === 'http://127.0.0.1:8080/index.html')
-      asideUrl = null;
-   if (location.href.includes('http://127.0.0.1:8080/page/js'))
-      asideUrl ='/page/components/js-aside.html';
-   if (location.href.includes('http://127.0.0.1:8080/page/dom'))
-      asideUrl = '/page/components/dom-aside.html';
-   if (location.href.includes('http://127.0.0.1:8080/page/java'))
-      asideUrl = '/page/components/java-aside.html';
-   return asideUrl;
+      return null;
+   let asideName = location.href.split('/')[4];
+   return `/page/components/${asideName}-aside.html`;
 }
 
-async function setAside(asideUrl) {
-   if(asideUrl){
-      let aside = document.createElement('aside');
-      aside.setAttribute('id', 'aside');
-      aside.innerHTML = asideUrl;
+async function setAside(asideText) {
+   if(asideText){
+      let aside = document.createElement('div');
+      aside.innerHTML = asideText;
       document.querySelector('nav').after(aside);
       return aside;
    }
 }
 
-function setCurrentUrl(aside, asideUrl, nav) {
+function setCurrentUrl(aside, nav) {
    aside.querySelectorAll('a').forEach(function (a) {
       if (location.href.includes(a.href)) {
          a.style.color = 'white';
          a.style.background = 'orangered';
       }
    });
-   toggleByUrl(aside);
-   if(asideUrl === '/page/components/js-aside.html') {
-      nav.querySelector('.javascript').style.background = 'black';
-   } else if(asideUrl === '/page/components/dom-aside.html') {
-      nav.querySelector('.html').style.background = 'black';
-   } else if(asideUrl === '/page/components/java-aside.html') {
-      nav.querySelector('.java').style.background = 'black';
-   } else{
-      nav.querySelector('.home').style.background = 'black';
-   }
+   let id = document.querySelector('aside').id;
+   nav.querySelector(`.${id}`).style.background = 'black';
 }
 
 function toggleByUrl(aside) {
-   document.querySelectorAll('aside ul li ul')
-      .forEach(toggle => toggle.hidden = true);
-   let position = location.href.split('/')[5];
-   let selectedLink = aside.querySelector('[href*='+`"${position}"`+']');
+   let findHref = location.href.split('/')[5];
+   let selectedLink = aside.querySelector('a[href*='+`"${findHref}"`+']');
    selectedLink.closest('ul').hidden = false;
 }
 
 function toggleHide(e) {
    if (e.target.tagName !== 'LI') return;
-   document.querySelectorAll('aside ul li ul')
-      .forEach(toggle => toggle.hidden = true);
    let contentToggle = e.target.querySelector('ul');
    contentToggle.hidden = !contentToggle.hidden;
-   return new Promise((resolve, reject) => {
-      resolve(document.querySelector('aside'))
-   })
 }
